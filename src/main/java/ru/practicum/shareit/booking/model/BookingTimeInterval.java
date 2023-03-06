ebe8faa0deb2c80cda9ae995ecff263eb14d0cca
Viacheslav <andreyev.vv@gmail.com>
2023-03-07 01:24:24 +0300

    sprint 16 commit 1.4

--- src/main/java/ru/practicum/shareit/booking/model/BookingTimeInterval.java
+++
@@ -1,5 +0,0 @@
-package ru.practicum.shareit.booking.model;
-
-public enum BookingTimeInterval {
-    ALL, PAST, CURRENT, FUTURE
-}
--- src/main/java/ru/practicum/shareit/exceptions/WrongEnumStatusException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class WrongEnumStatusException extends HttpCustomException {
-    public WrongEnumStatusException(HttpStatus status, String message) {
-        super(status, message);
-    }
-}
